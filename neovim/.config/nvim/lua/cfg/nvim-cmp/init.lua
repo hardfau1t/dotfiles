f334a6b29c5a8cf1a569c01897155556f36e8206
hardfau18 <the.qu1rky.b1t@gmail.com>
2022-03-15 19:22:10 +0530

    remapped snippet shortcut key

--- neovim/.config/nvim/lua/cfg/nvim-cmp/init.lua
+++ neovim/.config/nvim/lua/cfg/nvim-cmp/init.lua
@@ -1,73 +1,86 @@
 local mod = {}
 
 mod.setup = function()
-	local status, ls, cmp, lspkind
-    status, ls= pcall(require, "luasnip")
-	if not status then
-		print("luasnip is not installed")
-		return
-	end
-    status, cmp= pcall(require, "cmp")
-	if not status then
-		print("nvim-cmp is not installed")
-		return
-	end
-    status, lspkind= pcall(require, "lspkind")
-	if not status then
-		print("lspkind is not installed")
-		return
-	end
-	cmp.setup({
-		snippet = {
-			expand = function(args)
-				ls.lsp_expand(args.body)
-			end,
-		},
-		preselect = cmp.PreselectMode.None,
-		mapping = {
-			["<C-j>"] = cmp.mapping.scroll_docs(-4),
-			["<C-k>"] = cmp.mapping.scroll_docs(4),
-			["<C-s>"] = cmp.mapping.confirm({ select = true }),
-			["<C-n>"] = cmp.mapping(
-				function(fallback) -- if completion available go to next,else if snippets available next item
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif ls.jumpable(1) then
-						ls.jump(1)
-					else
-						fallback()
-					end
-				end
-			),
-			["<C-p>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				elseif ls.jumpable(-1) then
-					ls.jump(-1)
-				else
-					fallback()
-				end
-			end),
-			["<C-e>"] = cmp.mapping.close(),
-		},
+    local status, ls, cmp, lspkind
+    status, ls = pcall(require, "luasnip")
+    if not status then
+        print("luasnip is not installed")
+        return
+    end
+    status, cmp = pcall(require, "cmp")
+    if not status then
+        print("nvim-cmp is not installed")
+        return
+    end
+    status, lspkind = pcall(require, "lspkind")
+    if not status then
+        print("lspkind is not installed")
+        return
+    end
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                ls.lsp_expand(args.body)
+            end,
+        },
+        preselect = cmp.PreselectMode.None,
+        mapping = {
+            ["<C-j>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-k>"] = cmp.mapping.scroll_docs(4),
+            ["<C-s>"] = cmp.mapping(function(fallback)
+                if ls.jumpable(1) then
+                    print("jumpable jumping")
+                    ls.jump(1)
+                elseif cmp.visible() then
+                    print("printing")
+                    cmp.confirm({ select = true })
+                else
+                    fallback()
+                end
+            end),
+            ["<C-S>"] = cmp.mapping(function(fallback)
+                if ls.jumpable(-1) then
+                    ls.jump(-1)
+                else
+                    fallback()
+                end
+            end),
+            ["<C-n>"] = cmp.mapping(
+                function(fallback) -- if completion available go to next,else if snippets available next item
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    else
+                        fallback()
+                    end
+                end
+            ),
+            ["<C-p>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                else
+                    fallback()
+                end
+            end),
+            ["<C-c>"] = cmp.mapping.close(),
+        },
 
-		sources = {
-			{ name = "luasnip", max_item_count = 4 },
-			{ name = "nvim_lsp", max_item_count = 8 },
-			{ name = "nvim_lua" },
-			{ name = "neorg" },
-			{ name = "git" },
-			{ name = "path" },
-			{ name = "buffer", keyword_length = 4 },
-		},
-		formatting = {
-			format = lspkind.cmp_format(),
-		},
-		experimental = {
-			native_menu = false,
-			ghost_text = false,
-		},
-	})
+        sources = {
+            { name = "luasnip", max_item_count = 4 },
+            { name = "nvim_lsp", max_item_count = 8 },
+            { name = "nvim_lua" },
+            { name = "neorg" },
+            { name = "git" },
+            { name = "path" },
+            { name = "buffer", keyword_length = 4 },
+        },
+        formatting = {
+            format = lspkind.cmp_format(),
+        },
+        experimental = {
+            native_menu = false,
+            ghost_text = false,
+        },
+    })
 end
 
 return mod
