f56a6e319e054a5087098c5c95d5a8fdb65ff988
hardfau18 <the.qu1rky.b1t@gmail.com>
2022-03-24 13:18:37 +0530

    added folding

--- neovim/.config/nvim/lua/cfg/treesitter/init.lua
+++ neovim/.config/nvim/lua/cfg/treesitter/init.lua
@@ -1,36 +1,39 @@
 local mod = {}
 function mod.setup()
-	local present, ts = pcall(require, "nvim-treesitter.configs")
-	if not present then
-		print("treesitter is not installed")
-		return
-	end
-	ts.setup({
-		ensure_installed = {
-			"rust",
-			"c",
-			"python",
-			"norg",
-			"norg_meta",
-			"norg_table",
-			"lua",
-			"bash",
-			"html",
-			"css",
-			"yaml",
-			"toml",
-			"json",
-		},
-		highlight = {
-			enable = true,              -- false will disable the whole extension
-		},
-		indent = {
-			enable = false,
-		},
-		incremental_selection = {
-			enable = true,
-		},
-	})
+    local present, ts = pcall(require, "nvim-treesitter.configs")
+    if not present then
+        print("treesitter is not installed")
+        return
+    end
+
+    vim.o.foldmethod = "expr"
+    vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+    ts.setup({
+        ensure_installed = {
+            "rust",
+            "c",
+            "python",
+            "norg",
+            "norg_meta",
+            "norg_table",
+            "lua",
+            "bash",
+            "html",
+            "css",
+            "yaml",
+            "toml",
+            "json",
+        },
+        highlight = {
+            enable = true, -- false will disable the whole extension
+        },
+        indent = {
+            enable = false,
+        },
+        incremental_selection = {
+            enable = true,
+        },
+    })
 end
 
 return mod
