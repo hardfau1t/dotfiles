d6dab3195f244aa0bcf75f7f056e0cc2fe43a618
hardfau18 <the.qu1rky.b1t@gmail.com>
2022-05-30 21:29:49 +0530

    added gaming layout, added another monitor

--- qmk_keymaps/.local/share/qmk/keyboards/crkbd/keymaps/hardfau18/keymap.c
+++ qmk_keymaps/.local/share/qmk/keyboards/crkbd/keymaps/hardfau18/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             TG(7),             KC_Q,        KC_W,       KC_E,       KC_R,       KC_T,       /*------*/      KC_F,       KC_G,       KC_C,       KC_R,       KC_L,       KC_BSPC,
             KC_TAB,             KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       /*------*/      KC_D,       KC_H,       KC_T,       KC_N,       KC_S,       KC_MINS,
             KC_LSFT,            KC_SCLN,    KC_Q,       KC_J,       KC_K,       KC_X,       /*------*/      KC_B,       KC_M,       KC_W,       KC_V,       KC_Z,       KC_RSFT,
-                                    KC_LCTL,    KC_SPC,     KC_ENT, KC_ESC),                  /*------*/              LT(1,KC_SLSH),  LGUI_T(KC_ENT),  LALT_T(KC_BSLS)),
+                                    KC_LCTL,    KC_SPC, KC_ESC,                  /*------*/              LT(1,KC_SLSH),  LGUI_T(KC_ENT),  LALT_T(KC_BSLS)),
 };
 
 #ifdef OLED_DRIVER_ENABLE
