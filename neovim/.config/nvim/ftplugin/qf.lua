8590f10ef7c46a6641a93c7b3d8a04ca4ee16d35
hardfau1t <hardfau1t@proton.me>
2024-02-06 13:08:28 +0530

    refactor(neovim): remove quickfixlist shortcuts

--- neovim/.config/nvim/ftplugin/qf.lua
+++ neovim/.config/nvim/ftplugin/qf.lua
@@ -1,9 +1,5 @@
 local opts = { buffer = false, silent = true }
 
-vim.keymap.set("n", "<C-n>", ":cnext<CR>", opts)
-vim.keymap.set("n", "<C-p>", ":cprev<CR>", opts)
 vim.keymap.set("n", "<leader>qo", ":copen<CR>", opts)
 vim.keymap.set("n", "<leader>qq", ":cclose<CR>", opts)
-vim.keymap.set("n", "<leader>ql", ":cnewer<CR>", opts)
-vim.keymap.set("n", "<leader>qh", ":colder<CR>", opts)
 vim.keymap.set("n", "<esc>", ":cclose<CR>", opts)
