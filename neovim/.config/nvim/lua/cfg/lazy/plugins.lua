1d0f622bebde40127161570163617922ee665084
hardfau1t <hardfau1t@proton.me>
2024-02-06 05:20:55 +0000

    refactor(neovim): remove dashboard

--- neovim/.config/nvim/lua/cfg/lazy/plugins.lua
+++ neovim/.config/nvim/lua/cfg/lazy/plugins.lua
@@ -175,24 +175,6 @@ return {
     },
     { 'elkowar/yuck.vim', lazy = true, ft = { "yuck" } },
     {
-        'glepnir/dashboard-nvim',
-        event = 'VimEnter',
-        config = function()
-            local dashboard = require('dashboard')
-            local pokemon = require('pokemon')
-            pokemon.setup({
-                number = 'random',
-                size = 'auto',
-            })
-            dashboard.setup {
-                config = {
-                    header = pokemon.header()
-                }
-            }
-        end,
-        dependencies = { { 'nvim-tree/nvim-web-devicons', 'ColaMint/pokemon.nvim' } }
-    },
-    {
         "SmiteshP/nvim-navbuddy",
         dependencies = { {
             "neovim/nvim-lspconfig",
