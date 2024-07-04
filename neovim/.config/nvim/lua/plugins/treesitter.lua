bf6ab830379a3221c9fcaacd337acf504ccd0bd9
hardfau1t <hardfau1t@proton.me>
2024-07-04 10:22:37 +0530

    fix(neovim): tree sitter config missing highlight

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,36 @@
 local config = {
-        ensure_installed = {
-            "bash",
-            "c",
-            "cpp",
-            "css",
-            "html",
-            "json",
-            "lua",
-            "markdown",
-            "norg",
-            "python",
-            "rust",
-            "sql",
-            "toml",
-            "yaml",
-            "vim",
-            "vimdoc",
-        },
-        highlight = {
-            enable = true,
-        },
-        indent = {
-            enable = false,
-        },
-        incremental_selection = {
-            enable = true,
-        },
-    } 
+    ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "css",
+        "html",
+        "json",
+        "lua",
+        "markdown",
+        "norg",
+        "python",
+        "rust",
+        "sql",
+        "toml",
+        "yaml",
+        "vim",
+        "vimdoc",
+    },
+    highlight = {
+        enable = true,
+    },
+    indent = {
+        enable = false,
+    },
+    incremental_selection = {
+        enable = true,
+    },
+}
+
 return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
-        require("nvim-treesitter").setup(config)
+        require("nvim-treesitter.configs").setup(config)
     end,
 }
