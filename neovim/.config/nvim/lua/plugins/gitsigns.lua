ec364fe26eb8a1149de4d960dca9d7fe8e35e1fa
hardfau1t <hardfau1t@proton.me>
2024-07-19 17:31:59 +0530

    feat(nvim): show gitblame on file and hunk traversing

--- neovim/.config/nvim/lua/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ local function on_attach(bufnr)
     local gs = package.loaded.gitsigns
 
     local function map(mode, l, r, desc)
-        opts = {}
+        local opts = {}
         opts.buffer = bufnr
         if not desc ~= nil then
             opts.desc = desc
@@ -10,29 +10,21 @@ local function on_attach(bufnr)
         vim.keymap.set(mode, l, r, opts)
     end
 
-    -- Navigation
-    map('n', ']c', function()
-        if vim.wo.diff then return ']c' end
-        vim.schedule(function() gs.next_hunk() end)
-        return '<Ignore>'
-    end, "next hunk")
-
-    map('n', '[c', function()
-        if vim.wo.diff then return '[c' end
-        vim.schedule(function() gs.prev_hunk() end)
-        return '<Ignore>'
-    end, "previous hunk")
-
     -- Actions
+    map('n', '<leader>hn', function () gs.nav_hunk("next") end, "next hunk")
+    map('n', '<leader>hN', function () gs.nav_hunk("last") end, "last hunk")
+    map('n', '<leader>hp', function () gs.nav_hunk("prev") end, "last hunk")
+    map('n', '<leader>hP', function () gs.nav_hunk("first") end, "last hunk")
     map('n', '<leader>hs', gs.stage_hunk, "stage hunk")
-    map('n', '<leader>hr', gs.reset_hunk, "reset hunk")
     map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, "stage hunk")
+    map('n', '<leader>hr', gs.reset_hunk, "reset hunk")
     map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, "reset hunk")
     map('n', '<leader>hS', gs.stage_buffer, "stage buffer")
     map('n', '<leader>hu', gs.undo_stage_hunk, "undo stage hunk")
     map('n', '<leader>hR', gs.reset_buffer, "reset buffer")
     map('n', '<leader>hp', gs.preview_hunk, "preview hunk")
     map('n', '<leader>hb', gs.toggle_current_line_blame, "toggle line blame")
+    map('n', '<leader>hB', gs.toggle_current_line_blame, "Show git blame")
     map('n', '<leader>hd', gs.diffthis, "diff")
     map('n', '<leader>hD', function() gs.diffthis('~') end, "diff agains ~")
 
@@ -44,6 +36,7 @@ return {
     "lewis6991/gitsigns.nvim",
     opts = {
         on_attach = on_attach,
+        preview_config = {border = "single"},
         current_line_blame_formatter = '<author> : <abbrev_sha> : <author_time:%Y-%m-%d> - <summary>',
     }
 }
