af7a1d2e0732e1300e4bcd355c7368309ec0af94
hardfau1t <hardfau1t@proton.me>
2024-07-20 23:46:47 +0530

    fix(neovim): which-key proper setup

--- neovim/.config/nvim/lua/plugins/which_key.lua
+++ neovim/.config/nvim/lua/plugins/which_key.lua
@@ -12,10 +12,34 @@ local mappings = {
 
 return {
     "folke/which-key.nvim",
-    event = "VeryLazy",
-    config = function ()
-        local wk = require("which-key")
-        wk.setup(opts)
-        wk.register(mappings, {prefix = "<leader>"})
-    end
+    opts = {
+        spec = {
+             {"<leader>b", group = "buffer-management" },
+             {"<leader>c", group = "comment" },
+             {"<leader>d", group = "diagnostics" },
+             {"<leader>h", group = "git-hunks(neogit)" },
+             {"<leader>l", group = "lsp" },
+             {"<leader>o", group = "text-case" },
+             {"<leader>t", group = "telescope" },
+             {"<leader>s", group = "source" },
+             {"<leader>,", group = "snippets" },
+        }
+    },
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = true })
+            end,
+            desc = "Buffer Local Keymaps (which-key)"
+        },
+        {
+            "<C-W>",
+            function()
+                require("which-key").show({keys = "<c-w>", loop = true})
+            end
+        }
+    },
+    dependencies = {"echasnovski/mini.icons"}
+
 }
