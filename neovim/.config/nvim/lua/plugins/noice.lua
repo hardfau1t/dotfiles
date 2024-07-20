515dc68b285604d87324611832eb6191a1aefc5d
hardfau1t <hardfau1t@proton.me>
2024-07-21 01:10:11 +0530

    feat(neovim): adds noice.nvim. remove plugins file which is not used

---
+++ neovim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,19 @@
+return {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+            },
+        },
+        presets = {
+            lsp_doc_border = true,       -- add a border to hover docs and signature help
+        }
+    },
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+    }
+}
