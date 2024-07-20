5b85c21d2899b9ea4f5a9df302abf6725eac5757
hardfau1t <hardfau1t@proton.me>
2024-07-21 00:27:10 +0530

    fix(neovim): text-case use default key-mapping with leader

--- neovim/.config/nvim/lua/plugins/text_case.lua
+++ neovim/.config/nvim/lua/plugins/text_case.lua
@@ -2,7 +2,7 @@ return {
     "johmsalas/text-case.nvim",
     dependencies = { "nvim-telescope/telescope.nvim" },
     config = function()
-        require("textcase").setup({ default_keymappings_enabled = true, prefix = "<leader>o" })
+        require("textcase").setup({prefix = "<leader>o" })
         require("telescope").load_extension("textcase")
         vim.keymap.set('n', '<leader>to', '<cmd>TextCaseOpenTelescope<CR>', { desc = "TextCase convert" })
     end,
