540a54b0e76023b8570a0a030d72e3566a97f6f3
hardfau1t <hardfau1t@proton.me>
2024-11-06 16:12:48 +0530

    moved bat ipython to home-manager

--- home.nix
+++ home.nix
@@ -42,6 +42,7 @@
     nushellPlugins.polars
     pass
     pinentry-gtk2
+    python312Packages.ipython
     ripgrep
     slurp
     starship
@@ -70,6 +71,19 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
+    ".config/bat" = {
+      source = ./configs/bat;
+      recursive = true;
+    };
+    ".config/ipython/profile_default/ipython_config.py".text = ''
+c = get_config()  #noqa
+c.TerminalIPythonApp.display_banner = False
+c.InteractiveShell.pdb = False
+c.InteractiveShell.quiet = False
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.extra_open_editor_shortcuts = True
+c.TerminalInteractiveShell.quiet = True
+    '';
     ".config/nvim" = {
       source = ./configs/nvim;
       recursive = true;
