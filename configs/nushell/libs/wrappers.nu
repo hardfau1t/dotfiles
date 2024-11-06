622afdbb131d7363e65bdf2b342961e1d96c1314
hardfau1t <hardfau1t@proton.me>
2024-11-06 14:36:15 +0530

    adds home-manager configuration

---
+++ configs/nushell/libs/wrappers.nu
@@ -0,0 +1,45 @@
+
+alias actual-aws = aws
+alias actual-hyprctl = hyprctl
+alias actual-pactl = pactl
+alias actual-resolvectl = resolvectl
+alias actual-ip = ip
+
+export extern aws [...params: any] {
+    try {
+       actual-aws $params | from json
+    } catch {
+       actual-aws $params
+    }
+}
+
+export extern hyprctl [...params: any] {
+    try {
+       actual-hyprctl -j $params | from json
+    } catch {
+       actual-hyprctl $params
+    }
+}
+
+export extern resolvectl [...params] {
+    try {
+       actual-resolvectl --json=short $params | from json
+    } catch {
+       actual-resolvectl $params
+    }
+}
+
+export extern ip [...params] {
+    try {
+       actual-ip -j $params | from json
+    } catch {
+       actual-ip $params
+    }
+}
+
+export def --wrapped mitmproxy [...rest] {
+    ^mitmproxy --set $"confdir=($env.XDG_CONFIG_HOME)/mitmproxy" ...$rest
+}
+export def --wrapped mitmweb [...rest] {
+    ^mitmweb --set $"confdir=($env.XDG_CONFIG_HOME)/mitmproxy" ...$rest
+}
---
+++ home.nix
@@ -0,0 +1,115 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "gireesh";
+  home.homeDirectory = "/home/gireesh";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    alacritty
+    angband
+    aria2
+    bat
+    bottom
+    cliphist
+    dunst
+    dust
+    eww
+    fd
+    fuzzel
+    fzf
+    gnumake
+    grim
+    hledger
+    libnotify
+    maple-mono
+    mpc-cli
+    mpv
+    nil
+    nushellPlugins.polars
+    pass
+    pinentry-gtk2
+    ripgrep
+    slurp
+    starship
+    stow
+    tealdeer
+    wl-clipboard
+    zellij
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+    ".config/nushell/libs" = {
+      source = ./configs/nushell/libs;
+      recursive = true;
+    };
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/gireesh/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs = {
+    home-manager.enable = true;
+    nushell = {
+      enable = true;
+      configFile.source = ./configs/nushell/config.nu;
+      envFile.source = ./configs/nushell/env.nu;
+      loginFile.source = ./configs/nushell/login.nu;
+    };
+    carapace.enable = true;
+  };
+}
