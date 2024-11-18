38bc6d6e3b5f75fa08a6762dd27b6169f0be8df2
hardfau1t <hardfau1t@proton.me>
2024-11-18 15:58:38 +0530

    neovim: add lsp inlay hints toggle shortcut

--- configs/nvim/lua/cfg/lsp/keymaps.lua
+++ configs/nvim/lua/cfg/lsp/keymaps.lua
@@ -1,5 +1,14 @@
 local mod = {}
 
+local toggle_inlay_hints = function ()
+    if vim.lsp.inlay_hint.is_enabled() then
+        vim.lsp.inlay_hint.enable(false)
+    else
+        vim.lsp.inlay_hint.enable(true)
+    end
+
+end
+
 mod.setup = function(client, bufnr)
     vim.keymap.set("i", "<C-h>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { buffer = bufnr, silent = true, desc = "open lsp signature_help"})
     vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", { buffer = bufnr, silent = true, desc = "go to declaration"})
@@ -9,15 +18,16 @@ mod.setup = function(client, bufnr)
     vim.keymap.set('n', "<leader>lwr", function() vim.lsp.buf.remove_workspace_folder() end, { buffer = bufnr, desc = "remove workspace folder"})
     vim.keymap.set('n', "<leader>lwl",
         function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, { buffer = bufnr, desc = "list all workspace folders"})
-    vim.keymap.set('n', "<leader>lp", function() require 'cfg.lsp.utils'.PeekDefinition() end, { buffer = bufnr, desc = "peek definition"})
+    vim.keymap.set('n', "<leader>la", function() vim.lsp.buf.code_action() end, { buffer = bufnr, desc = "show available actions"})
+    vim.keymap.set('n', "<leader>li", toggle_inlay_hints, { buffer = bufnr, desc = "restart lsp server"})
     vim.keymap.set('n', "<leader>lh", function() vim.lsp.buf.hover() end, { buffer = bufnr, desc = "show hover information"})
+    vim.keymap.set('n', "<leader>lp", function() require 'cfg.lsp.utils'.PeekDefinition() end, { buffer = bufnr, desc = "peek definition"})
     vim.keymap.set('n', "<leader>ls", function() vim.lsp.buf.signature_help() end, { buffer = bufnr, desc = "show signature help"})
     vim.keymap.set('n', "<leader>lr", function() vim.lsp.buf.rename() end, { buffer = bufnr, desc = "rename variable"})
-    vim.keymap.set('n', "<leader>la", function() vim.lsp.buf.code_action() end, { buffer = bufnr, desc = "show available actions"})
     vim.keymap.set('n', "<leader>lt", function() vim.lsp.buf.type_definition() end, { buffer = bufnr, desc = "show type definition"})
     vim.keymap.set({'n', 'v'}, "<leader>lf", function() vim.lsp.buf.format({ async = true }) end, { buffer = bufnr, desc = "format section/whole file"})
     vim.keymap.set('n', "<leader>lT", "<cmd>LspStop<CR>", { buffer = bufnr, desc = "stop lsp server"})
-    vim.keymap.set('n', "<leader>li", "<cmd>LspInfo<CR>", { buffer = bufnr, desc = "start lsp server"})
+    vim.keymap.set('n', "<leader>lI", "<cmd>LspInfo<CR>", { buffer = bufnr, desc = "Lsp status"})
     vim.keymap.set('n', "<leader>lR", "<cmd>LspRestart<CR>", { buffer = bufnr, desc = "restart lsp server"})
     -- diagnostic keymaps
     vim.keymap.set('n', "<leader>do", vim.diagnostic.open_float, { buffer = bufnr , desc = "open diagnostic message in floating window"})
@@ -36,3 +46,4 @@ mod.setup = function(client, bufnr)
     end
 end
 return mod
+
