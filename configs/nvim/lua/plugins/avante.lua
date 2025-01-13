ff5ff6aa60b0d7524aa593485126bf16bb79507c
hardfau1t <hardfau1t@proton.me>
2025-01-13 16:53:03 +0530

    nvim: adds avante.nvim

---
+++ configs/nvim/lua/plugins/avante.lua
@@ -0,0 +1,21 @@
+return {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
+    opts = {
+        provider = "claude"
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        --- The below dependencies are optional,
+        "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        'MeanderingProgrammer/render-markdown.nvim',
+    },
+}
--- configs/nvim/lua/plugins/render_markdown.lua
+++ configs/nvim/lua/plugins/render_markdown.lua
@@ -1,8 +1,10 @@
 return {
     'MeanderingProgrammer/render-markdown.nvim',
-    ft = { "markdown" },
+    ft = { "markdown" , "Avante"},
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = {
+        file_types = {"markdown", "Avante"}
+    },
 }
