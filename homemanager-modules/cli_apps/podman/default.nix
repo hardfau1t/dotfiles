1482d93d68eb41270d1cb7faf11b98dea441d483
hardfau1t <hardfau1t@proton.me>
2025-01-28 08:25:55 +0530

    podman: set storage driver

--- homemanager-modules/cli_apps/podman/default.nix
+++ homemanager-modules/cli_apps/podman/default.nix
@@ -8,6 +8,7 @@
   config = lib.mkIf (osConfig.virtualisation.podman.enable && config ? containers-storage ) {
     xdg.configFile."containers/storage.conf".text = ''
       [storage]
+      driver = "overlay2"
       graphroot = "${config.containers-storage}"
     '';
   };
