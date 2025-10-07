42bf2f1c02a587c2b97e4142ad97d9a06f542ca0
hardfau1t <hardfau1t@proton.me>
2025-10-07 16:28:03 +0530

    feat(dunst): play notification sound

--- homemanager-modules/gui_apps/dunst/default.nix
+++ homemanager-modules/gui_apps/dunst/default.nix
@@ -1,4 +1,15 @@
-{lib, config, ...}: 
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 lib.mkIf config.services.dunst.enable {
-    xdg.configFile."dunst/dunstrc".source = config.lib.file.mkOutOfStoreSymlink "${config.dots_dir}/homemanager-modules/gui_apps/dunst/dunstrc";
+  xdg.configFile."dunst/dunstrc".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.dots_dir}/homemanager-modules/gui_apps/dunst/dunstrc";
+  home.packages = [
+    pkgs.sound-theme-freedesktop
+    # this script is used in dustrc to play the sound everytime notification is recieved
+    (pkgs.writeShellScriptBin "play-notification-sound" "pw-play ${pkgs.sound-theme-freedesktop}/share/sounds/freedesktop/stereo/complete.oga")
+  ];
 }
