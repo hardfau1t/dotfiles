77cafef87ddfda492c0b18bd5f339854da80600b
hardfau1t <hardfau1t@proton.me>
2026-04-10 09:24:59 +0530

    feat(home): add rust cargo and rustup paths

--- homemanager-modules/cli_apps/default.nix
+++ homemanager-modules/cli_apps/default.nix
@@ -82,6 +82,13 @@
       };
 
     };
+    home = {
+      sessionVariables = {
+        CARGO_HOME = "${config.xdg.dataHome}/cargo";
+        RUSTUP_HOME = "${config.xdg.dataHome}/rustup";
+      };
+      sessionPath = [ "$CARGO_HOME/bin" "$RUSTUP_HOME/bin"];
+    };
     services = {
       udiskie.enable = lib.mkDefault true;
     };
