c61d099e5d183db3a1184a40ebbc3f43ed28fa31
hardfau1t <hardfau1t@proton.me>
2026-07-25 09:00:55 +0530

    refactor(pi-agent): move to unstable version instead of latest

--- homemanager-modules/cli_apps/assistants/pi-agent/default.nix
+++ homemanager-modules/cli_apps/assistants/pi-agent/default.nix
@@ -1,14 +1,34 @@
 {
   config,
   lib,
-  pkgs,
+  unstable,
   ...
 }:
+# let
+#   pi = pkgs.pi-coding-agent.overrideAttrs (
+#     final: prev: rec {
+#       version = "0.82.0";
+#       src = pkgs.fetchFromGitHub {
+#         owner = "earendil-works";
+#         repo = "pi";
+#         tag = "v${version}";
+#         hash = "sha256-oKm0nyGmRY6rlQGMODB8DteMTVUUMroy/YXPphoxrvY=";
+#       };
+#       npmDeps = pkgs.fetchNpmDeps {
+#         inherit src;
+#         # Use a fake hash here first, let the build fail to get the real one
+#         hash = "sha256-3oqrN/uguYfkUHlfmKGxnLIvUo484IMGlydz6p9o/Dw="; 
+#       };
+#     }
+#   );
+# in
 {
   options = {
     custom.assistants.pi-agent.enable = lib.mkEnableOption "Enable coding assistants";
   };
   config = lib.mkIf config.custom.assistants.pi-agent.enable {
-    home.packages = [pkgs.pi-coding-agent];
+    home.packages = [
+      unstable.pi-coding-agent
+    ];
   };
 }
